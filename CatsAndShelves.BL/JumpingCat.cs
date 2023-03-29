de70cb1b47bbdb443eb1c0eb9c1707310c542c52
jesse <jessefortuin850@gmail.com>
2023-03-29 15:57:51 +0200

    Feat: Changed do while loop to while loop

--- CatsAndShelves.BL/JumpingCat.cs
+++ CatsAndShelves.BL/JumpingCat.cs
@@ -4,54 +4,53 @@ namespace CatsAndShelves.BL
 {
     public class JumpingCat
     {
-        //public int JumpsNeeded(int start, int finish)
-        //{
-        //    int jump = start;
-
-        //    int jumpCounter = 0;
-
-        //    do
-        //    {
-        //        if (jump + 3 <= finish)
-        //        {
-        //            jump += 3;
-
-        //            jumpCounter++;
-        //        }
-
-        //        else
-        //        {
-        //            jump += 1;
-
-        //            jumpCounter++;
-        //        }
-        //    } while (jump != finish);
-        //    return jumpCounter;
-        //}
- 
         public int JumpsNeeded(int start, int finish)
         {
-            List<int> jumpsRecorder= new List<int>();
+            int jumpCounter = 0;
 
-            JumpsNeededRecorder(start, finish, 0, jumpsRecorder);
-            return jumpsRecorder.Min();
-        }
-
-        public void JumpsNeededRecorder(int start, int finish, int numberOfJumps, List<int> jumpsRecorder)
-        {
-
-            if (start == finish)
+            while (start != finish)
             {
-                //success condition
-                jumpsRecorder.Add(numberOfJumps);
+                if (start + 3 <= finish)
+                {
+                    start += 3;
+
+                    jumpCounter++;
+                }
+
+                else
+                {
+                    start += 1;
+
+                    jumpCounter++;
+                }
             }
 
-            if (start < finish)
-            {
-                JumpsNeededRecorder(start + 1, finish, numberOfJumps + 1, jumpsRecorder);
-
-                JumpsNeededRecorder(start + 3, finish, numberOfJumps + 1, jumpsRecorder);              
-            }
+            return jumpCounter;
         }
+
+        //public int JumpsNeeded(int start, int finish)
+        //{
+        //    List<int> jumpsRecorder= new List<int>();
+
+        //    JumpsNeededRecorder(start, finish, 0, jumpsRecorder);
+        //    return jumpsRecorder.Min();
+        //}
+
+        //public void JumpsNeededRecorder(int start, int finish, int numberOfJumps, List<int> jumpsRecorder)
+        //{
+
+        //    if (start == finish)
+        //    {
+        //        //success condition
+        //        jumpsRecorder.Add(numberOfJumps);
+        //    }
+
+        //    if (start < finish)
+        //    {
+        //        JumpsNeededRecorder(start + 1, finish, numberOfJumps + 1, jumpsRecorder);
+
+        //        JumpsNeededRecorder(start + 3, finish, numberOfJumps + 1, jumpsRecorder);              
+        //    }
+        //}
     }
 }
--- README.md
+++ README.md
@@ -27,3 +27,9 @@ Find the minimum number of jumps to go from start to finish
 
 Example
 Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+
+-------------------------------------------------
+Solutions
+
+
+
